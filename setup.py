4bc9bd024d33b796e4178f113dc0b6d8085e82ab
i-wizard <njoagwuanidavid@gmail.com>
2025-02-19 18:53:58 +0100

    add GitHub link to  setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     description="A collection of internal tools shared among all Centric Microservices",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    url="https://github.com/davu-ai/centric_tools",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
